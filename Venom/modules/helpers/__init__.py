a0933facc7dd98df58fc1ec32b7eaf96c8d6d556
ʙᴀᴅ ᴍᴜɴᴅᴀ 👻 <singhsukhwinder07990@gmail.com>
2025-07-11 14:32:59 +0000

    Update __init__.py

--- Venom/modules/helpers/__init__.py
+++ Venom/modules/helpers/__init__.py
@@ -20,3 +20,21 @@ def is_admins(func: Callable) -> Callable:
 
 from .inline import *
 from .read import *
+
+
+
+#GIMNI
+from motor.motor_asyncio import AsyncIOMotorClient
+import config
+
+# Database connection
+ChatBot = AsyncIOMotorClient(config.MONGO_URL)
+db = ChatBot["ChatBot"]  # Database
+usersdb = db["users"]    # Users Collection
+chatsdb = db["chats"]    # Chats Collection
+
+# Import functions for use in other parts of the application
+from .admin import *
+from .shizu import *
+from .chatbot import
+*
